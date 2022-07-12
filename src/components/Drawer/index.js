101400dfed264d4d55701ebc1987ac7b64772b98
izaanjahangir <izaanjahangir2@gmail.com>
2022-07-12 16:03:38 +0500

    small fix

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import auth from '@react-native-firebase/auth';
 
 import authActions from '../../redux/auth/action';
@@ -11,6 +11,7 @@ import style from './style';
 
 const Drawer = props => {
   const dispatch = useDispatch();
+  const user = useSelector(store => store.auth.user);
   const logout = () => {
     auth().signOut();
     dispatch(authActions.clearUser());
@@ -19,12 +20,13 @@ const Drawer = props => {
     );
   };
 
+  console.log(user)
   return (
     <View style={style.container}>
       <View style={style.topContainer}>
         <Avatar />
-        <Text style={style.welcomeText}>Welcome izaan</Text>
-        <Text style={style.emailText}>izaanjahangir2@gmail.com</Text>
+        <Text style={style.welcomeText}>Welcome {user?.firstName}</Text>
+        <Text style={style.emailText}>{user?.email}</Text>
       </View>
       <View style={style.bottomContainer}>
         <Button onPress={logout} containerStyle={style.logoutButton}>
